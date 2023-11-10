09a911bd84e7442b1b54a67dc45ab1d085a5fa5b
Cristian <cristian91493@gmail.com>
2023-11-09 22:05:28 -0500

    first day's progress

---
+++ fishing.js
@@ -0,0 +1,5 @@
+function updateProgressBar(progressBar, value){
+    value = Math.round(value)
+    progressBar.querySelector(".progress__fill").style.width = `${value}%`
+    progressBar.querySelector(".progress__text").textContent = `${value/10} Secs`
+}

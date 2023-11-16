faafc9a5befdcbe44132964aa83bf4cc5d4c98bb
Cristian <cristian91493@gmail.com>
2023-11-16 00:54:54 -0500

    added chance and identified some problems

--- fishing.js
+++ fishing.js
@@ -6,25 +6,98 @@ function updateProgressBar(progressBar, value, percentage){
 
 pBar = document.querySelector(".progress")
 
-//setinterval
 
-// console.log(pBar)
 let timeLeft= 100
 let timeToFour= 0
 
+function zeroToFour(){
+setInterval(function(){
+    if(timeToFour<=30){
+                timeToFour+= 10
+                updateProgressBar(pBar, timeToFour,timeToFour)   
+            }},1000)}
+            
+            // need to make this not count twice or keep stacking the countdown 
+function tenToZero(){
+setInterval(function(){
+    if(timeLeft>=1){
+        timeLeft= timeLeft-10
+        console.log(timeLeft)
+        updateProgressBar(pBar, timeLeft, timeLeft)  
+        
 
-//need to put the lines below into functions to be called on later
-//also should make a progress bar for each one or find a way to use the same for both
-
-// setInterval(function zeroToFour(){
-//     if(timeToFour<=30){
-//                 timeToFour+= 10
-//                 updateProgressBar(pBar, timeToFour,timeToFour*2.5)   
-//             }},1000)
+    }
+},1000 )
+}
 
 
-// setInterval(function tenToZero(){
-//     if(timeLeft>=1){
-//         timeLeft= timeLeft-10
-//         updateProgressBar(pBar, timeLeft, timeLeft)   
-//     }},1000) 
+ripple = document.querySelector(".ripple")
+bobber = document.querySelector(".bobber")
+splash = document.querySelector(".splash")
+fishingRod= document.querySelector(".fishing-rod")
+progress= document.querySelector(".progress")
+
+function castingForFish(){
+    
+        bobber.style.visibility="visible"
+        fishingRod.style.visibility = "visible"
+        progress.style.visibility = "visible"
+        tenToZero()
+        setTimeout(()=>{
+            if(timeLeft <= 0){
+            bobber.style.visibility="hidden"
+            fishingRod.style.visibility = "hidden"
+            progress.style.visibility="hidden"}
+            timeLeft= 100
+        },10000)
+}
+
+
+
+function fishOnHook(){ 
+    let chance= Math.floor(Math.random()*13-3)
+    console.log(chance)
+    if(chance >= 0){
+        setTimeout(()=>{
+            bobber.style.visibility = "hidden"
+            progress.style.visibility = "hidden"
+            splash.style.visiblity = "visible"
+            fishingRod.style.animationName = "fish-on-hook"
+            
+
+        },chance*1000)
+    }
+    splash.addEventListener("click",() => {
+        window.location.href = "puzzle.html";
+        
+    })
+}
+
+// fishOnHook()
+
+function gamePlay(){
+    ripple.addEventListener("click", () => {
+        castingForFish()
+        // fishOnHook()
+    })
+    }
+    gamePlay()
+//     gamePlay()
+// function gamePlay(){
+// ripple.addEventListener("click", () => {
+//     bobber.style.visibility="visible"
+//     fishingRod.style.visibility = "visible"
+//     progress.style.visibility = "visible"
+//     tenToZero()
+//     setTimeout(()=>{
+//         if(timeLeft <= 0){
+//         bobber.style.visibility="hidden"
+//         fishingRod.style.visibility = "hidden"
+//         progress.style.visibility="hidden"}
+//         timeLeft= 100
+//     },10000)
+// })
+// }           
+
+    
+    
